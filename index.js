e4df4f1b7b4f6035870d6cc75e0d12e7d2b36d34
iancdavis <iancdavis7@gmail.com>
2021-01-29 12:57:39 -0700

    Configured puppeteer for linux on 32bit Raspian

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const puppeteer = require('puppeteer')
 const CronJob = require('cron').CronJob
+const {performance} = require('perf_hooks')
 
 
 const url_bestbuy = 'https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324'
 // const url_bestbuy = 'https://www.amazon.com/Sony-WH-1000XM4-Canceling-Headphones-phone-call/dp/B0863TXGM3/'
 
 async function configureBrowser() {
-    const browser = await puppeteer.launch({headless: false})
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: '/usr/bin/chromium-browser',
+        userDataDir: '/home/pi/programming/web_projects/isXboxInStock/puppeteer_data'
+    })
     const page = await browser.newPage()
     page.setDefaultNavigationTimeout(0) //bestbuy loads super slow
 
@@ -14,7 +19,7 @@ async function configureBrowser() {
     await page.setRequestInterception(true)
 
     page.on('request', request => {
-        if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet'){
+        if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet' || request.resourceType() == 'font'){
             request.abort()
         } else {
             request.continue()
@@ -26,12 +31,19 @@ async function configureBrowser() {
 }
 
 async function checkStock(page) {
-    await page.reload()
+    const t0 = performance.now()
+    await page.reload({waitUntil: 'domcontentloaded'})
+    const t1 = performance.now()
+    console.log('page reload took ' + (t1 - t0) + ' milliseconds.')
     // let html = await page.evaluate(() => document.body.innerHTML)
+    const evalT0 = performance.now()
     let buttonText = await page.$eval('.add-to-cart-button', el => el.innerText)
-    // console.log(buttonText)
+    const evalT1 = performance.now()
+    console.log('Eval took ' + (evalT1 - evalT0) + ' milliseconds.')
+
+    console.log(buttonText)
     if (buttonText == 'Sold Out'){
-        console.log('The Xbox Series X is sold out at bestbuy.com')
+        console.log('The Xbox Series X is sold out at bestbuy.com. ')
     } else {
         console.log('The Xbox Series X might be in stock at bestbuy.com')
     }
